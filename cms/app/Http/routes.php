4b17e42795a8b4e389d3732c5006df4f321125f7
miyakey <miyasaviola@gmail.com>
2017-07-15 17:41:57 +0000

    first commit

---
+++ cms/app/Http/routes.php
@@ -0,0 +1,46 @@
+<?php
+use App\Books; 
+use Illuminate\Http\Request; 
+
+/** 
+* 本 のダッシュボード 表示 
+*/ 
+Route::get('/', function () {
+    
+    $books = Books::orderBy('created_at', 'asc')->get();
+    return view('books', ['books' => $books]);
+	 //
+//	 return view('books');
+}); 
+
+/**
+* 新 「本」 を 追加 
+*/ 
+Route::post('/books', function (Request $request) {
+    //バリデーション
+    $validator = Validator::make($request->all(), [
+            'item_name' => 'required|max:255',
+    ]);
+    //バリデーション:エラー
+    if ($validator->fails()) {
+            return redirect('/')
+                ->withInput()
+                ->withErrors($validator);
+    }
+        // Eloquent モデル
+    $books = new Books;
+    $books->item_name = $request->item_name;
+    $books->item_number = '1';
+    $books->item_amount = '1000';
+    $books->published = '2017-03-07 00:00:00';
+    $books->save();   //「/」ルートにリダイレクト 
+    return redirect('/');
+}); 
+
+/**
+* 本 を 削除 
+*/ 
+Route::delete('/books/{book}', function (Books $book) {
+    $book->delete();
+    return redirect('/');
+});
---
+++ cms/resources/views/books.blade.php
@@ -0,0 +1,74 @@
+<!-- resources/views/books.blade.php -->
+
+@extends('layouts.app')
+
+@section('content')
+    
+    <!-- Bootstrap の定形コード... -->
+    
+    <div class="panel-body">
+        <!-- バリデーションエラーの表示に使用-->
+        @include('common.errors')
+        <!-- バリデーションエラーの表示に使用-->
+        
+        <!-- 本登録フォーム -->
+        <form action="{{ url('books') }}" method="POST" class="form-horizontal">
+            {{ csrf_field() }}
+            
+            <!-- 本のタイトル -->
+            <div class="form-group">
+                <label for="book" class="col-sm-3 control-label">Book</label>
+                
+                <div class="col-sm-6">
+                    <input type="text" name="item_name" id="book-name"class="form-control">
+                </div>
+            </div>
+            
+            <!-- 本 登録ボタン -->
+            <div class="form-group">
+                <div class="col-sm-offset-3 col-sm-6">
+                    <button type="submit" class="btn btn-default">
+                        <i class="glyphicon glyphicon-plus"></i> Save
+                    </button>
+                </div>
+            </div>
+        </form>
+        @if (count($books) > 0)
+        <div class="panel panel-default">
+            <div class="panel-heading">現在の本 </div>
+            <div class="panel-body">
+                <table class="table table-striped task-table">
+                    <!-- テーブルヘッダ -->
+                    <thead> <th>本一覧</th>
+                        <th>&nbsp;</th>
+                    </thead>
+                    <!-- テーブル本体 -->
+                    <tbody>
+                        @foreach ($books as $book)
+                        <tr>
+                            <!-- 本タイトル -->
+                            <td class="table-text">
+                                <div>{{ $book->item_name }}
+                                </div>
+                            </td>
+                            <td>
+                                <form action="{{ url('book/'.$book->id) }}" method="POST">
+                                    {{ csrf_field() }}
+                                    {{ method_field('DELETE') }}
+                                    <button type="submit" class="btn btn-danger"> <i class="glyphicon glyphicon-trash"></i> 
+                                        削除
+                                    </button>
+                                </form>
+                            </td>
+                        </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    @endif
+<!-- Book: 既に登録されてる本のリスト -->
+    </div>
+    
+    <!-- Book: 既に登録されてる本のリスト -->
+@endsection
