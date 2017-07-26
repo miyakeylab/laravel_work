c3c6af438ba0344d41c695d89578b706affbdcb2
miyakey <miyasaviola@gmail.com>
2017-07-26 16:48:57 +0000

    リファクタリング

--- cms/resources/views/booksedit.blade.php
+++ cms/resources/views/booksedit.blade.php
@@ -7,32 +7,28 @@
 								<!-- item_name	-->
 								<div	class="form-group">
 											<label	for="item_name">Title</label>
-											<input	type="text"	id="item_name"	name="item_name"	
-class="form-control"	value="{{$book->item_name}}">
+											<input	type="text"	id="item_name"	name="item_name" class="form-control"	value="{{$book->item_name}}">
 								</div>
 									<!--/	item_name	-->
 								
 								<!-- item_number	-->
 								<div	class="form-group">
 											<label	for="item_number">Number</label>
-								<input	type="text"	id="item_number"	name="item_number"	
-class="form-control"	value="{{$book->item_number}}">
+								<input	type="text"	id="item_number"	name="item_number" class="form-control"	value="{{$book->item_number}}">
 								</div>
 							 <!--/	item_number	-->
 								<!-- item_amount	-->
 								<div	class="form-group">
 											<label	for="item_amount">Amount</label>
-								<input	type="text"	id="item_amount"	name="item_amount"	
-class="form-control"	value="{{$book->item_amount}}">
+								<input	type="text"	id="item_amount"	name="item_amount" class="form-control"	value="{{$book->item_amount}}">
 								</div>
 								<!--/	item_amount	-->
 								
 								<!-- published	-->
-								<div	class="form-group">
-											<label	for="published">published</label>
-												<input	type="datetime"	id="published"	name="published"	
-class="form-control"	value="{{$book->published}}"/>
-					</div>
+								<div class="form-group">
+									<label	for="published">published</label>
+									<input	type="datetime"	id="published"	name="published" class="form-control"	value="{{$book->published}}"/>
+								</div>
 					<!--/	published	-->
 					
 		<!-- Save ボタン/Back ボタン -->
